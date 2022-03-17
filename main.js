76747ecbc37c52cbe898c408a1e404654bb1826f
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-03-17 17:53:28 +0900

    修正⑥

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 let getBtn = document.getElementById("checkButton");
 getBtn.addEventListener("click",function getValue() {
    console.log("JS");
-        const FizzNum = document.getElementById('FizzNum');
-        const BuzzNum = document.getElementById('BuzzNum');
-        const getFizzValue = FizzNum.value;
-        const getBuzzValue = BuzzNum.value;
+        const FizzNum = document.getElementById('FizzNum').value;
+        const BuzzNum = document.getElementById('BuzzNum').value;
        for(let i=1; i<100; i++){
-           let Fizz = getFizzValue;
-           let Buzz = getBuzzValue;
-           let FizzBuzz = Fizz * Buzz;  
-            if( i%Fizz ===0　&& i%Buzz ===0) {
-                console.log(`FizzBuzz\b${FizzBuzz}`);
-            } else if (i%Fizz===0 ) {
-                 console.log(`Fizz\b${Fizz}`);
-            } else if (i%Buzz===0 ) {
-                console.log(`Buzz\b${Buzz}`);
+            if( i%FizzNum ===0　&& i%BuzzNum ===0) {
+                console.log(`FizzBuzz\b${i}`);
+            } else if (i%FizzNum===0 ) {
+                 console.log(`Fizz\b${i}`);
+            } else if (i%BuzzNum===0 ) {
+                console.log(`Buzz\b${i}`);
              }
          }
     } );
