7af4c88958bba4da73a9f7826da506b5d579f4c0
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-03-17 22:56:40 +0900

    修正⑧

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 let getBtn = document.getElementById("checkButton");
 console.log("JS");
-   
-   getBtn.addEventListener("click",function getValue() {
+    getBtn.addEventListener("click",function getValue() {
     const FizzNum = parseFloat(document.getElementById('FizzNum').value);
     const BuzzNum = parseFloat(document.getElementById('BuzzNum').value);
     let err = "整数値を入力してください！";
     let Perfect = ""
     if(FizzNum === "" || BuzzNum === "") {
         document.getElementById('error').textContent = err;
-    }else if(Number.isInteger(FizzNum) !== 0 && Number.isInteger(BuzzNum) !==0){
+    }else if(Number.isInteger(FizzNum) !== true && Number.isInteger(BuzzNum) !== true){
         document.getElementById('error').textContent = err ;
         document.getElementById('error_b').textContent = err ;
-    }else {
-     document.getElementById('error').textContent = Perfect;
-  }
-  
-       for(let i=1; i<100; i++){
-            if( i%FizzNum ===0　&& i%BuzzNum ===0) {
-                console.log(`FizzBuzz\b${i}`);
-            } else if (i%FizzNum===0 ) {
-                 console.log(`Fizz\b${i}`);
-            } else if (i%BuzzNum===0 ) {
-                console.log(`Buzz\b${i}`);
-             }
-         }
+    }else if(Number.isInteger(FizzNum) !== false  && Number.isInteger(BuzzNum) !== false){
+        document.getElementById('error').textContent = Perfect;
+        document.getElementById('error_b').textContent = Perfect;
+    for(let i=1; i<100; i++){
+        if( i%FizzNum ===0　&& i%BuzzNum ===0) {
+            console.log(`FizzBuzz\b${i}`);
+        } else if (i%FizzNum===0 ) {
+            console.log(`Fizz\b${i}`);
+        } else if (i%BuzzNum===0 ) {
+            console.log(`Buzz\b${i}`);
+        }
+    }
+}       
     } );
