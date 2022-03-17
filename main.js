8a99789ac30708c9ce68b73e91aa9c179cf64cd1
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-03-17 22:40:47 +0900

    修正⑥

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 let getBtn = document.getElementById("checkButton");
-getBtn.addEventListener("click",function getValue() {
-   console.log("JS");
-        const FizzNum = document.getElementById('FizzNum').value;
-        const BuzzNum = document.getElementById('BuzzNum').value;
+console.log("JS");
+   
+   getBtn.addEventListener("click",function getValue() {
+    const FizzNum = parseFloat(document.getElementById('FizzNum').value);
+    const BuzzNum = parseFloat(document.getElementById('BuzzNum').value);
+    let err = "整数値を入力してください！";
+    let Perfect = ""
+    if(FizzNum === "" || BuzzNum === "") {
+        document.getElementById('error').textContent = err;
+    }else if(Number.isInteger(FizzNum) !== 0 && Number.isInteger(BuzzNum) !==0){
+        document.getElementById('error').textContent = err ;
+        document.getElementById('error_b').textContent = err ;
+    }else {
+     document.getElementById('error').textContent = Perfect;
+  }
+  
        for(let i=1; i<100; i++){
             if( i%FizzNum ===0　&& i%BuzzNum ===0) {
                 console.log(`FizzBuzz\b${i}`);
