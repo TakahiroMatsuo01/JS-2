ce1b032b0edf809e38d896375f35801d8ac4cce2
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-04-01 05:03:59 +0900

    修正➓

--- main.js
+++ main.js
@@ -1,30 +1,43 @@
-let getBtn = document.getElementById("checkButton");
-console.log("JS");
+const getBtn = document.getElementById("checkButton");
+    console.log("JS");
+    
     getBtn.addEventListener("click",function getValue() {
     const FizzNum = parseFloat(document.getElementById('FizzNum').value);
     const BuzzNum = parseFloat(document.getElementById('BuzzNum').value);
-    let err = "整数値を入力してください！";
-    let Perfect = ""
+
+    
     if(FizzNum === "" || BuzzNum === "") {
-        document.getElementById('error').textContent = err;
-    }else if(Number.isInteger(FizzNum) !== true && Number.isInteger(BuzzNum) !== true){
-        document.getElementById('error').textContent = err ;
-        document.getElementById('error_b').textContent = err ;
-    }else if(Number.isInteger(FizzNum) !== false  && Number.isInteger(BuzzNum) !== false){
-        document.getElementById('error').textContent = Perfect;
-        document.getElementById('error_b').textContent = Perfect;
+        
+        document.getElementById('error').textContent = "整数値を入力してください！";
+
+    } else if(Number.isInteger(FizzNum) !== true && Number.isInteger(BuzzNum) !== true) {
+        
+        document.getElementById('error').textContent = "整数値を入力してください！" ;
+        document.getElementById('error_b').textContent = "整数値を入力してください！" ;
+
+    } else if(Number.isInteger(FizzNum) !== false  && Number.isInteger(BuzzNum) !== false) {
+
+        document.getElementById('error').textContent = "";
+        document.getElementById('error_b').textContent = "";
+
     for(let i=1; i<100; i++){
-        const newDiv = document.createElement("p");
+            const newDiv = document.getElementById("div1").createElement("p");
         if( i%FizzNum ===0　&& i%BuzzNum ===0) {
-            let Fizz_buzzlog ="FizzBuzz\b"+`${i}`;
+
+            let Fizz_buzzlog ="FizzBuzz\b\n"+`${i}`;
             div1.appendChild(newDiv).textContent= Fizz_buzzlog;
+        
         } else if (i%FizzNum===0 ) {
-            let Fizz_log ="Fizz\b"+`${i}`;
+
+            let Fizz_log = "Fizz\b\n"+`${i}`;
             div1.appendChild(newDiv).textContent= Fizz_log;
+        
         } else if (i%BuzzNum===0 ) {
-            let Buzz_log ="Fizz\b"+`${i}`;
+            
+            let Buzz_log = "Buzz\b\n"+`${i}`;
             div1.appendChild(newDiv).textContent = Buzz_log;
+        
+            }
         }
-    }
-}       
-    } );
+    }       
+});
