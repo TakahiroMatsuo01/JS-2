ba67953d3b7bb9ea83d1b64cb595ebf5d8887e42
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-04-04 23:30:10 +0900

    修正11

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
 const getBtn = document.getElementById("checkButton");
 console.log("JS");
-    getBtn.addEventListener("click",function getValue() {
-    const FizzNum = parseFloat(document.getElementById('FizzNum').value);
-    const BuzzNum = parseFloat(document.getElementById('BuzzNum').value);
+getBtn.addEventListener("click",function getValue() {
+const FizzNum = parseFloat(document.getElementById('FizzNum').value);
+const BuzzNum = parseFloat(document.getElementById('BuzzNum').value);
 
-    if(FizzNum === "" || BuzzNum === "" ) {
-        document.getElementById('error').textContent = "整数値を入力してください！"  ;
-    } else if(Number.isInteger(FizzNum) !== true && Number.isInteger(BuzzNum) !== true){
+if(FizzNum === "" || BuzzNum === "" ) {
+    document.getElementById('error').textContent = "整数値を入力してください！"  ;
+} else if(Number.isInteger(FizzNum) !== true && Number.isInteger(BuzzNum) !== true){
 
-        document.getElementById('error').textContent = "整数値を入力してください！" ;
-        document.getElementById('error_b').textContent = "整数値を入力してください！" ;
+    document.getElementById('error').textContent = "整数値を入力してください！" ;
+    document.getElementById('error_b').textContent = "整数値を入力してください！" ;
 
-    } else if(Number.isInteger(FizzNum) !== false  && Number.isInteger(BuzzNum) !== false){
+} else if(Number.isInteger(FizzNum) !== false  && Number.isInteger(BuzzNum) !== false){
 
-        document.getElementById('error').textContent = "";
-        document.getElementById('error_b').textContent = "";
+    document.getElementById('error').textContent = "";
+    document.getElementById('error_b').textContent = "";
 
-        for(let i=1; i<100; i++){
-        const div1 = document. getElementById('div1')
-        const newDiv = document.createElement('p');
-        if( i%FizzNum ===0　&& i%BuzzNum ===0) {
+    for(let i = 1; i < 100; i++){
+    const div1 = document. getElementById('div1')
+    const newDiv = document.createElement('div');
+    if( i % FizzNum === 0　&& i % BuzzNum === 0) {
 
-            let Fizz_buzzlog ="FizzBuzz\b"+`${i}`;
-            div1.appendChild(newDiv).textContent= Fizz_buzzlog;
-    
-        } else if (i%FizzNum===0 ) {
-    
-            let Fizz_log ="Fizz\b"+`${i}`;
-            div1.appendChild(newDiv).textContent= Fizz_log;
-    
-        } else if (i%BuzzNum===0 ) {
-    
-            let Buzz_log ="Fizz\b"+`${i}`;
-            div1.appendChild(newDiv).textContent = Buzz_log;
+        let Fizz_buzzlog ="FizzBuzz\b"+`${i}`;
+        div1.appendChild(newDiv).textContent= Fizz_buzzlog;
 
+    } else if (i % FizzNum === 0 ) {
+
+        let Fizz_log ="Fizz\b"+`${i}`;
+        div1.appendChild(newDiv).textContent= Fizz_log;
+
+    } else if ( i % BuzzNum === 0 ) {
+
+        let Buzz_log ="Fizz\b"+`${i}`;
+        div1.appendChild(newDiv).textContent = Buzz_log;
+
+            }
         }
-    }
-}       
-    } );
+    }       
+} );
